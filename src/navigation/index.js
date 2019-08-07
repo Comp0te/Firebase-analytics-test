c189d78d844c5f592e745104e1a807cb9304aab3
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-08-07 16:39:37 +0300

    Configure automatic setCurrentScreen when route change

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { View } from 'react-native';
 import { createAppContainer, createStackNavigator } from 'react-navigation';
 
-import Screen1 from '../screens/Screen1'
-import Screen2 from '../screens/Screen2'
+import Screen1 from '../screens/Screen1';
+import Screen2 from '../screens/Screen2';
+import * as firebase from 'react-native-firebase';
 
 const Navigator = createStackNavigator({
   screen1: Screen1,
@@ -14,12 +15,39 @@ const Navigator = createStackNavigator({
 
 const NavigatorContainer = createAppContainer(Navigator);
 
-const AppNavigator = () => {
-  return (
-    <View style={{ flex: 1 }}>
-      <NavigatorContainer/>
-    </View>
-  );
-};
+class AppNavigator extends React.Component {
+  getActiveRouteName(navigationState) {
+    if (!navigationState) {
+      return null;
+    }
+
+    const route = navigationState.routes[navigationState.index];
+    // Dive into nested navigators
+    if (route.routes) {
+      return this.getActiveRouteName(route);
+    }
+
+    return route.routeName;
+  }
+
+  onNavigationStateChange = (prevState, currentState) => {
+    const currentScreen = this.getActiveRouteName(currentState);
+    const prevScreen = this.getActiveRouteName(prevState);
+
+    if (prevScreen !== currentScreen) {
+      firebase.analytics().setCurrentScreen(currentScreen);
+    }
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <NavigatorContainer
+          onNavigationStateChange={this.onNavigationStateChange}
+        />
+      </View>
+    );
+  }
+}
 
 export default AppNavigator;
